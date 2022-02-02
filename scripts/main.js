667e8b2704fbb938d1dfb2c26082975b35468b08
Chelsea.harris <chelsea.harris@asurion.com>
2022-02-02 16:03:20 -0600

    styling progress

--- scripts/main.js
+++ scripts/main.js
@@ -6,25 +6,31 @@ import { findService } from "./services.js"
 const mainContainer = document.querySelector("#container") // reffrences  the ID by using the #. this helps link the CSS
 
 const applicationHTML = `
-<h1>Cider Park</h1>
-<article class="details">
-    <section class="detail--column list details__park">
-        <h2>Park Divisions</h2>
-        ${findParks()}
-    </section>
+    <div id="header">
+    <img class="logo" alt="park" src= "https://www.cedarparktexas.gov/home/showpublishedimage/8511/636487491632730000" />
+    <h1>Cider Park</h1>
+    </div>
+    <article class="details">
+        <section class="detail--column list details__park">
+            <h2>Park Divisions</h2>
+            ${findParks()}
+        </section>
     </article>
-    <article class="details">
-    <section class="detail--column list details__services">
-        <h2>Services</h2>
-        ${findService()}
-    </section>
-</article>
-    <article class="details">
-    <section class="detail--column list details__services">
-        <h2>Welcome Our Guest</h2>
-        ${findGuest()}
-    </section>
-</article>
+    <div class="details--container">
+        <article class="details">
+            <section class="detail--column list details__services">
+                <h2>Services</h2>
+                ${findService()}
+            </section>
+        </article>
+
+        <article class="details">
+            <section class="detail--column list details__services">
+                <h2>Welcome Our Guest</h2>
+                ${findGuest()}
+            </section>
+        </article>
+    </div>
 
 
 `
